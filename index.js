149f3f4738b7b0aded307bf64d48d2b260b09f79
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-05-29 14:29:41 +0200

    Added menu db #7

---
+++ index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+const express = require('express');
+const app = express();
+const PORT = 1337;
+
+// Ska alla ligga i egen mapp?
+const nedb = require('nedb-promise');
+// Databas för kaffemenyn
+const coffeeDB = new nedb({ filename: 'coffeMenu.db', autoload: true });
+// Databas för users och ordrar kopplade till user. Om inte inloggad - lägg order under guest?
+const usersDB = new nedb({ filename: 'users.db', autoload: true });
+
+function createMenu() {
+    const coffees = JSON.parse(fs.readFileSync('menu.json'));
+
+    coffees.menu.forEach(product => {
+        coffeeDB.insert({ product });
+    });
+}
+// cresateMenu();
+
+app.listen(PORT, () => {
+    console.log('Listening on port', PORT)
+});
