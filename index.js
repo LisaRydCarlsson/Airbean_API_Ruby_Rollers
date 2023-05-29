b7a4df1e806adc2c38b49c8c1c0e2c4d670b1760
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-05-29 15:29:56 +0200

    Adding order to db #10

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const PORT = 1337;
 
 // Ska alla ligga i egen mapp?
 const nedb = require('nedb-promise');
+const { userInfo } = require('os');
 // Databas för kaffemenyn
 const coffeeDB = new nedb({ filename: 'coffeMenu.db', autoload: true });
 // Databas för users och ordrar kopplade till user. Om inte inloggad - lägg order under guest?
 const usersDB = new nedb({ filename: 'users.db', autoload: true });
+
+app.use(express.json());
 
 function createMenu() {
     const coffees = JSON.parse(fs.readFileSync('menu.json'));
@@ -18,13 +21,22 @@ function createMenu() {
         coffeeDB.insert({ product });
     });
 }
-// cresateMenu();
+// createMenu();
 
-app.get('/api/beans/', async (req, res) => {
+app.get('/api/beans', async (req, res) => {
     const menu = await coffeeDB.find({});
     res.json(menu);
 });
 
+// Skapa middleware här
+app.post('/api/beans/order', (req, res) => {
+    const order = req.body;
+
+    usersDB.insert(order);
+
+    res.json(order)
+});
+
 app.listen(PORT, () => {
-    console.log('Listening on port', PORT)
+    console.log('Listening on port', PORT);
 });
